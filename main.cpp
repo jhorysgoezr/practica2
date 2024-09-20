1dd823d111f5f251b2b0041f493416263f53bb47
JhorysGoezR <jhorys.goezr@udea.edu.co>
2024-09-20 14:43:13 -0500

    Se agrego el switch y nombres de problemas

--- main.cpp
+++ main.cpp
@@ -4,6 +4,125 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int opcion=1;
+    cout<<endl;
+    cout<<"***Bienvednido a la practica 1***"<<endl;
+    while (opcion!=0){
+        cout<<"seleccione una opcion"<<endl;
+        cout<<"1) identificador vocal o consonante"<<endl;
+        cout<<"2) Generador de letras"<<endl;
+        cout<<"3) Comparador de cadenas de caracteres"<<endl;
+        cout<<"4) Convertir cadena de caracteres a numeros"<<endl;
+        cout<<"5) Convertir numero a cadena de caracteres"<<endl;
+        cout<<"6) Convertir caracteres minuscula a mayuscula"<<endl;
+        cout<<"7) Eliminar caracteres repetidos"<<endl;
+        cout<<"8) Separador de cadena de caracteres y numeros"<<endl;
+        cout<<"9) Separador de cadena de caracteres numericos"<<endl;
+        cout<<"10) Convertidor de numero romano a aragbico"<<endl;
+        cout<<"11) Reservas de cine"<<endl;
+        cout<<"12) Verificador matriz cuadrado magico"<<endl;
+        cout<<"13) Encontrar estrellas"<<endl;
+        cout<<"14) Rotador de matriz"<<endl;
+        cout<<"15) Interseccion entre rectangulos"<<endl;
+        cout<<"16) Calculador de caminos en una cuadricula"<<endl;
+        cout<<"17) Numeros amigables"<<endl;
+        cout<<"17) Permutaciones lexicograficas"<<endl;
+        cin>>opcion;
+        switch(opcion){
+        case 1:
+        {
+
+        }
+        break;
+        case 2:{
+
+        }
+        break;
+        case 3:
+        {
+
+        }
+        break;
+        case 4:
+        {
+
+        }
+        break;
+        case 5:
+        {
+
+        }
+        break;
+        case 6:
+        {
+
+        }
+        break;
+        case 7:
+        {
+
+        }
+        break;
+        case 8:
+        {
+
+        }
+        break;
+        case 9:
+        {
+
+        }
+        break;
+        case 10:
+        {
+
+        }
+        break;
+        case 11:
+        {
+
+        }
+        break;
+        case 12:
+        {
+
+        }
+        break;
+        case 13:
+        {
+
+        break;
+        case 14:
+        {
+
+        }
+        break;
+        case 15:
+        {
+
+        }
+        break;
+        case 16:
+        {
+
+        }
+        break;
+        case 17:
+        {
+
+        }
+        break;
+        case 18:
+        {
+
+        }
+        break;
+        default:
+            if (opcion!=0)
+                cout<<"opcion no valida"<<endl;
+            break;
+        }
+    }
     return 0;
+    }
 }
